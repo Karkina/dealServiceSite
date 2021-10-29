3a7fa3a478f3d4f1c52ce0d814f3ab299af5b84b
karkina <nico.soumare@gmail.com>
2021-10-29 15:27:38 +0200

    test

--- snippets/models.py
+++ snippets/models.py
@@ -17,7 +17,7 @@ class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     nom = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
-    montant = models.DecimalField(decimal_places=2, max_digits=950)
+    montant = models.DecimalField(decimal_places=2,default=0, max_digits=950)
     devise = models.CharField(choices=DEVISE,default="Eur",max_length=20)
     zone = models.CharField(default="0",choices=ZONES, max_length=100)
     borower = models.CharField(default="0",max_length=100)
